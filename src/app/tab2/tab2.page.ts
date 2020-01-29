c9a12f721ae05f0723a8d47aad3f5633b3f371b4
unknown <yatin696@gmail.com>
2020-01-29 23:03:08 +0530

    Creating ionic form

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -1,12 +1,22 @@
 import { Component } from '@angular/core';
-
+import {Validators, FormBuilder, FormGroup,ReactiveFormsModule } from '@angular/forms';
 @Component({
   selector: 'app-tab2',
   templateUrl: 'tab2.page.html',
   styleUrls: ['tab2.page.scss']
 })
 export class Tab2Page {
+  private todo : FormGroup;
+ 
 
-  constructor() {}
+  constructor( private formBuilder: FormBuilder ) {
+    this.todo = this.formBuilder.group({
+      title: ['', Validators.required],
+      description: [''],
+    });
+  }
+  logForm(){
+    console.log(this.todo.value)
+  }
 
 }
